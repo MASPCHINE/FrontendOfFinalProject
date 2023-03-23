cae63c7dd43239b5d9d8a42d0ab07b1eee15bc26
TARIK BAYRAM <tkbayram7@gmail.com>
2023-03-23 10:34:00 +0300

    Added : Filter by Category

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,18 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+
 import { Product } from '../models/product';
-import { ProductResponseModel } from '../models/productResponseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  apiUrl = 'https://localhost:7094/api/products/getall';
+  apiUrl = 'https://localhost:7094/api/';
 
-  constructor(private htppClient: HttpClient) { }
+  constructor(private htppClient: HttpClient) {}
 
-  getProducts():Observable<ProductResponseModel> {
-    return this.htppClient.get<ProductResponseModel>(this.apiUrl)
+  getProducts(): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + 'products/getall';
+    return this.htppClient.get<ListResponseModel<Product>>(newPath);
+  }
+  getProductsByCategory(categoryId: number): Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + 'products/getbycategory?categoryId='+categoryId;
+    return this.htppClient.get<ListResponseModel<Product>>(newPath);
   }
 }
